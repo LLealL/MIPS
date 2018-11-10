231adea93496513b9a07a3006889a1931d9fbac6
Lucas <30868253+LLealL@users.noreply.github.com>
2018-11-10 02:12:34 -0200

    Update fileWrite.asm

--- Files/fileWrite.asm
+++ Files/fileWrite.asm
@@ -1,5 +1,5 @@
 .data
-	file: .asciiz "/home/keyalisth/Desktop/Repositorios/MIPS/Files/data.dat"
+	file: .asciiz "/.../MIPS/Files/data.dat" #specify your Directory
 	buffer: .space  32
 	msgInputTxt: .asciiz "Enter with your string data"
 	OpenfileErr: .asciiz "Failed to open File [syscall failed]\n"
